a990e296a4bce8a650334abf1a4894214b8ffbe8
Kashish jain <165054560+kashijain@users.noreply.github.com>
2025-06-21 16:10:05 +0530

    Temp rename to __temp

---
+++ app/api/user/data/route.js
@@ -0,0 +1,26 @@
+import connectDB from "@/config/db";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import User from "@/models/User"; 
+
+export async function GET(request) {
+  try {
+    const { userId } = await getAuth(request); 
+
+    if (!userId) {
+      return NextResponse.json({ success: false, message: "Unauthorized" }, { status: 401 });
+    }
+
+    await connectDB();
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return NextResponse.json({ success: false, message: "User Not Found" });
+    }
+
+    return NextResponse.json({ success: true, user });
+  } catch (error) {
+    return NextResponse.json({ success: false, message: error.message });
+  }
+}
